85fabdacd105271d60081a9a5fe39ffe7f1d0d3d
Ethan Bower <ethanbower@Ethans-Macbook-Pro.local>
2026-05-10 19:31:13 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: process.env.BASE_PATH || 'out',
   basePath: process.env.BASE_PATH || '',
   images: {
     unoptimized: true,
