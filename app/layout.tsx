7b65f454347b6f801c4c2b19997939a96ebb38d2
Ethan Bower <ethanbower@Ethans-Macbook-Pro.local>
2026-05-23 17:48:07 -0400

    fixed full screen for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,9 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode;}>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
