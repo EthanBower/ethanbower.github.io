f4abb322bd0262401e385bf96c6514cb245f193e
Ethan Bower <ethanbower@Ethans-Macbook-Pro.local>
2026-05-30 09:47:40 -0400

    fixed typescript variants type issue

--- lib/components/global/navigationMenu.tsx
+++ lib/components/global/navigationMenu.tsx
@@ -1,96 +1,29 @@
 "use client";
 
 import Image from "next/image";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import { useState } from "react";
 
-const navbarVariants = {
-  initial: {
-    y: 120,
-    scale: .8
-  },
-  enter: { 
-    y: 0, 
-    scale: 1,
-    transition: { 
-      type: "spring", 
-      stiffness: 140, 
-      damping: 6, 
-      mass: 0.8
-    }
-  },
-  exit: { 
-    y: 150, 
-    scale: 0.85,
-    transition: { 
-      type: "spring", 
-      stiffness: 180, 
-      damping: 22, 
-      mass: 0.8,
-      delay: .5
-    }
-  }
+const navbarVariants: Variants = {
+  initial: { y: 120, scale: .8 },
+  enter: { y: 0, scale: 1, transition: { type: "spring", stiffness: 140, damping: 6, mass: 0.8 } },
+  exit: { y: 150, scale: 0.85, transition: { type: "spring", stiffness: 180, damping: 22, mass: 0.8, delay: .5 } }
 };
 
-const containerVariants = {
-  enter: {
-    transition: {
-      delayChildren: 0.25,
-      staggerChildren: 0.1,
-      staggerDirection: 1,
-    }
-  },
-  exit: {
-    transition: {
-      delayChildren: 0.25,
-      staggerChildren: 0.1,
-      staggerDirection: -1,
-    }
-  }
+const containerVariants: Variants = {
+  enter: { transition: { delayChildren: 0.25, staggerChildren: 0.1, staggerDirection: 1 } },
+  exit: { transition: { delayChildren: 0.25, staggerChildren: 0.1, staggerDirection: -1 } }
 };
 
-const toolTipVariants = {
-  initial: {
-    opacity: 0,
-    y: 0,
-    scale: 0.95,
-    filter: "blur(10px)" 
-  },
-  enter: {
-    opacity: 1,
-    y: -43,
-    scale: 1,
-    filter: "blur(0px)"
-  },
-  exit: {
-    opacity: 0,
-    y: 0, 
-    scale: 0.95, 
-    filter: "blur(10px)"
-  }
+const toolTipVariants: Variants = {
+  initial: { opacity: 0, y: 0, scale: 0.95, filter: "blur(10px)" },
+  enter: { opacity: 1, y: -43, scale: 1, filter: "blur(0px)" },
+  exit: { opacity: 0, y: 0, scale: 0.95, filter: "blur(10px)" }
 };
 
-const itemVariants = {
-  enter: {
-    opacity: 1,
-    y: 0,
-    scale: 1,
-    transition: {
-      type: "spring",
-      stiffness: 260,
-      damping: 18,
-    },
-  },
-  exit: {
-    opacity: 0,
-    y: 30,
-    scale: 0.8,
-    transition: {
-      type: "spring",
-      stiffness: 260,
-      damping: 18,
-    }
-  }
+const itemVariants: Variants = {
+  enter: { opacity: 1, y: 0, scale: 1, transition: { type: "spring", stiffness: 260, damping: 18, } },
+  exit: { opacity: 0, y: 30, scale: 0.8, transition: { type: "spring", stiffness: 260, damping: 18 } }
 };
 
 interface NavItem {
--- lib/components/global/popupWindow.tsx
+++ lib/components/global/popupWindow.tsx
@@ -2,9 +2,11 @@
 
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const ANIMATION_TIME_MS = 0.3;
+const popupWindowBlurVariant: Variants = { hidden: { opacity: 0, filter: "blur(10px)" }, visible: { opacity: 1, filter: "blur(0px)" }, exit: { opacity: 0, filter: "blur(10px)" } };
+const popupWindowScaleVariant: Variants = { hidden: { scale: 1.5, y: 0 }, visible: { scale: 1, y: 0 }, exit: { scale: 0.8, y: 0 } };
 
 type PopupWindowProps = Readonly<{
     windowTitle: string;
@@ -45,8 +47,8 @@ export default function PopupWindow({ windowTitle, onClose, children }: PopupWin
     return (
         <div ref={windowRef} className="absolute inset-0 flex items-center justify-center pointer-events-none">
             <motion.div className="z-10 pointer-events-auto" drag dragConstraints={windowRef} dragMomentum={true} dragElastic={0.02} whileDrag={{ scale: 1.03 }} >
-                <motion.div className="popup-window shadow-2xl" variants={{ hidden: { opacity: 0, filter: "blur(10px)" }, visible: { opacity: 1, filter: "blur(0px)" }, exit: { opacity: 0, filter: "blur(10px)" } }} initial="hidden" animate={visible ? "visible" : "exit"} transition={{ duration: ANIMATION_TIME_MS }} >
-                    <motion.div variants={{ hidden: { scale: 1.5, y: 0 }, visible: { scale: 1, y: 0 }, exit: { scale: 0.8, y: 0 } }} initial="hidden" animate={visible ? "visible" : "exit"} transition={{ duration: ANIMATION_TIME_MS, ease: [0.16, 1, 0.3, 1] }} >
+                <motion.div className="popup-window shadow-2xl" variants={popupWindowBlurVariant} initial="hidden" animate={visible ? "visible" : "exit"} transition={{ duration: ANIMATION_TIME_MS }} >
+                    <motion.div variants={popupWindowScaleVariant} initial="hidden" animate={visible ? "visible" : "exit"} transition={{ duration: ANIMATION_TIME_MS, ease: [0.16, 1, 0.3, 1] }} >
                         <div className="cursor-grab active:cursor-grabbing select-none flex items-center gap-4 p-4 pb-2">
                             <Image src="/double-arrow.svg" alt="Terminal Icon" width={24} height={24} priority />
                             <div className="flex flex-col text-left">
